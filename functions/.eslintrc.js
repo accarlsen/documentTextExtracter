5cee3af52683c609e9253ef9fa676954ad3cd48f
accarlsen <alexander.carlsen@outlook.com>
2023-05-13 13:28:49 +0200

    added console logs

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,14 +4,6 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
@@ -24,9 +16,4 @@ module.exports = {
     "@typescript-eslint",
     "import",
   ],
-  rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
 };
