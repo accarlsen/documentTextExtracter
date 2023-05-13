d1354d4f7ee0fdb3f52da1a0713b7b04368f5d76
Gaute Rønning <gaute.ronning@norskdatateknikk.no>
2023-05-13 13:04:47 +0200

    gcloud: add extractDocument function (WIP)
    Signed-off-by: Gaute Rønning <gaute.ronning@norskdatateknikk.no>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const functions = require("firebase-functions");
+const logger = require("firebase-functions/logger");
+const Busboy = require('busboy');
+const Readable = require('stream')
+const mammoth = require("mammoth");
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +20,27 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.extractDocument = functions.onRequest(async (request: Request, response: Response) => {
+    const fileHandler =  Busboy({ headers: request.headers })
+    // Handle fileread
+    fileHandler.on('file', (fieldname : string, file : typeof Readable, filename : string, encoding : string, mimetype : string) => {
+        if(mimetype === "application/pdf") {
+            file.on('data', (data : Buffer) => {
+                
+            })
+        }
+        else if(mimetype === "application/msword" || mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+            file.on('data', (data : Buffer) => {
+                mammoth.extractRawText({buffer: data}).then((function(result : { value: string}) {
+                    const text : string = result.value
+                    console.log(text)
+                    return {text: text}
+                })).catch((err: Error) => {
+                    console.log(err)
+                    throw new Error('Failed to extract text from Word document');
+                })
+            })
+        }
+    })
+})
