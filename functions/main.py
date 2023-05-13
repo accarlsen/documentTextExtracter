8fbde2fe61b72f99dc2f52988289603095ae47e3
Gaute Rønning <gaute.ronning@norskdatateknikk.no>
2023-05-13 14:33:33 +0200

    it works
    Signed-off-by: Gaute Rønning <gaute.ronning@norskdatateknikk.no>

--- functions/main.py
+++ functions/main.py
@@ -1,5 +1,13 @@
 import requests
+from werkzeug.utils import secure_filename
+import mammoth
+import fitz
+from flask import Flask, request
+import io
 
+app = Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
 def hello_world(request):
     """HTTP Cloud Function.
     Args:
@@ -13,26 +21,30 @@ def hello_world(request):
     return 'Hello, World!'
 
 
+@app.route('/extract_document', methods=['POST'])
 def extract_document(request):
     if 'file' not in request.files:
         return 'No file part in the request', 400
     file = request.files['file']
     filename = secure_filename(file.filename)
     mimetype = file.mimetype
-
     print(f'File: {filename}')
     print(f'Mimetype: {mimetype}')
-
     if mimetype == "application/pdf":
-        reader = PdfFileReader(file)
-        text = reader.getPage(0).extractText()
-        print('PDF detected')
-        print(text)
-    elif mimetype in ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]:
-        with file:
-            b = bytearray(file.read())
-            result = mammoth.extract_raw_text(io.BytesIO(b))
-            text = result.value
+        doc = fitz.open(stream=file.read(), filetype='pdf')
+        texts = []
+        for page_num, page in enumerate(doc):
+            text = page.get_text()
+            print('PDF detected')
             print(text)
+            texts.append(text)
+        return {texts: texts}, 200
+    elif mimetype in ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/octet-stream"]:
+        print("Testing file read")
+        b = bytearray(file.read())
+        result = mammoth.extract_raw_text(io.BytesIO(b))
+        text = result.value
+        print(text)
+        return {texts: text}, 200
 
-    return 'Success', 200
+    return "Error processing file", 400
